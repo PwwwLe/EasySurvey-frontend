c1b029422c3119bc8651ebd3900c8871d621e15a
Sliver_0812 <pwwwle0812@gmail.com>
2024-07-12 10:09:26 +0800

    微调代码，修改错误

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
-import { unauthorized } from "@/net";
-import { useStore } from "@/store";
+import {createRouter, createWebHistory} from "vue-router";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -93,7 +91,7 @@ const router = createRouter({
             ]
         },
         {
-            path:"/reateQuestionnaire",
+            path: "/createQuestionnaire",
             name: "createQuestionnaire",
             component: () => import('@/views/admin/createQuestionnaire.vue')
         }
