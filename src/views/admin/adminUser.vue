9c67a946f98554853bd7735eda1c75af76ddc964
Sliver_0812 <pwwwle0812@gmail.com>
2024-07-13 14:50:16 +0800

    完成多选框逻辑

--- src/views/admin/adminUser.vue
+++ src/views/admin/adminUser.vue
@@ -1,9 +1,34 @@
 <script setup>
 import {onMounted, reactive, ref} from 'vue'
 import axios from "axios";
-import {ElMessage} from "element-plus";
+import {ElMessage, ElMessageBox} from "element-plus";
 
-let userData = ref([])
+let userData = ref([
+  {
+    name: '企业1',
+    president: '张三',
+    email: 'pwwwle@gmail.com',
+    business_scope: 'IT',
+    create_time: '2021-09-01',
+    update_time: '2021-09-01'
+  },
+  {
+    name: '企业2',
+    president: '李四',
+    email: 'rtyuiohg@qq.com',
+    business_scope: 'IT',
+    create_time: '2021-09-01',
+    update_time: '2021-09-01'
+  },
+  {
+    name: '企业3',
+    president: '王五',
+    email: 'sbfnioeaibfi@163.com',
+    business_scope: 'IT',
+    create_time: '2021-09-01',
+    update_time: '2021-09-01'
+  },
+])
 let total = ref(0)
 const currentPage = ref(1)
 const pageSize = ref(10)
@@ -38,7 +63,7 @@ const handleCurrentChange = (val) => {
 };
 
 // 弹出添加窗口可见行
-const dialogVisible = ref(false);
+//let dialogVisible = ref(false);
 
 // 新增企业信息
 const user = reactive({
@@ -48,6 +73,7 @@ const user = reactive({
 
 // 管理员添加企业的方法
 const handleAdd = async () => {
+  console.log("add")
   try {
     const response = await axios.post('http://47.121.187.213:8080/admin/addUser', {
       name: user.name,
@@ -69,11 +95,55 @@ const handleAdd = async () => {
   }
 };
 
+const selectedUsers = ref([]);
+const userTableRef = ref(null);
+const dialogVisible = ref(false)
+
+const handleClose = (done) => {
+  ElMessageBox.confirm('确认关闭？')
+      .then(() => {
+        done()
+      })
+      .catch(() => {
+        ElMessage.info('取消关闭')
+      })
+}
+
+const handleSelectionChange = (selection) => {
+  selectedUsers.value = selection;
+  console.log('Selected users:', selectedUsers.value);
+};
+
+const clearSelections = () => {
+  userTableRef.value?.clearSelection();
+};
+
+const deleteUsers = () => {
+  console.log('delete users:', selectedUsers.value);
+};
+
 const handleEdit = () => {
   console.log('edit')
 }
-const handleDelete = () => {
+const handleDelete = async (id) => {
   console.log('delete')
+
+  try {
+    const response = await axios.delete(`http://47.121.187.213:8080/admin/deleteOneById`, {
+      params: {
+        id: id
+      }
+    });
+    if (response.status === 200) {
+      ElMessage.success("删除企业成功！");
+      await fetchUserData(currentPage.value, pageSize.value);
+    } else {
+      ElMessage.error("删除企业失败");
+    }
+  } catch (error) {
+    ElMessage.error("删除企业失败");
+    console.error('删除企业失败: ', error);
+  }
 }
 
 </script>
@@ -85,9 +155,9 @@ const handleDelete = () => {
     <template #header>
       <div class="card-header">
         <span>企业管理</span>
-        <el-button type="primary" size="small" plain @click="dialogVisible = true">添加</el-button>
+        <el-button type="primary" size="default" plain @click="dialogVisible=true">添加</el-button>
         <!-- 添加企业信息弹窗 -->
-        <el-dialog :visible.sync="dialogVisible" title="添加企业信息" draggable>
+        <el-dialog v-model="dialogVisible" title="添加企业信息" :before-close="handleClose" draggable>
           <el-form :model="user" label-width="120px">
             <el-form-item label="企业名称">
               <el-input v-model="user.name" placeholder="请输入企业名称"></el-input>
@@ -96,17 +166,18 @@ const handleDelete = () => {
               <el-input v-model="user.password" placeholder="请输入密码" type="password"></el-input>
             </el-form-item>
           </el-form>
-          <span slot="footer" class="dialog-footer">
+          <div class="dialog-footer">
             <el-button @click="dialogVisible = false">取消</el-button>
             <el-button type="primary" @click="handleAdd">确认</el-button>
-          </span>
+          </div>
         </el-dialog>
       </div>
     </template>
 
 
     <!-- 卡片内容 -->
-    <el-table :data="userData" style="width: 100%">
+    <el-table ref="userTableRef" :data="userData" style="width: 100% " @selection-change="handleSelectionChange">
+      <el-table-column fixed type="selection" width="55"/>
       <el-table-column fixed prop="name" label="名称" width="120"/>
       <el-table-column prop="president" label="代表人" min-width="120"/>
       <el-table-column prop="email" label="邮箱" min-width="200"/>
@@ -114,14 +185,21 @@ const handleDelete = () => {
       <el-table-column prop="create_time" label="创建时间" min-width="120"/>
       <el-table-column prop="update_time" label="更新时间" min-width="120"/>
       <el-table-column fixed="right" prop="operation" label="操作" min-width="100">
-        <template #default>
+        <template #default="scope">
           <div class="operation-buttons">
             <el-button link type="primary" size="small" @click="handleEdit">编辑</el-button>
-            <el-button link type="warning" size="small" @click="handleDelete">删除</el-button>
+            <el-button link type="warning" size="small" @click="handleDelete(scope.row.id)">删除</el-button>
           </div>
         </template>
       </el-table-column>
     </el-table>
+
+    <div style="margin-top: 20px">
+      <el-button size="default" @click="clearSelections">
+        重置选择
+      </el-button>
+      <el-button size="default" @click="deleteUsers" type="danger">批量删除</el-button>
+    </div>
 
     <!-- 卡片底部 -->
     <div class="pagination-block">
